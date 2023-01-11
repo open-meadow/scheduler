9c304605e4c255e4ae577f36542e4665bbfdb989
open-meadow <rohan.varughese@protonmail.com>
2023-01-11 17:00:02 -0500

    Initialize header and empty

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 // Button stories
 storiesOf("Button", module)
@@ -143,4 +144,9 @@ storiesOf("Appointment", module)
 .add("Appointment with Time", () => <Appointment time="12pm" />)
 .add("Header", () => (
   <Header time="12pm"/>
-));
+))
+.add("Empty", () => (
+  <Empty
+    onAdd = {action("onAdd")}
+  />
+))
