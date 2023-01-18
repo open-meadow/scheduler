aaa8a28a95742c4dcd93fd6ea633c6d133032ae2
open-meadow <rohan.varughese@protonmail.com>
2023-01-17 20:26:06 -0500

    Write more tests for application

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,13 +6,15 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   // console.log("daylistitem props:", props);
   let dayListItemClass = classNames("day-list__item", {
-    "day-list__item--selected" : props.selected,
-    "day-list__item--full" : (props.spots === 0)
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
   });
 
   const formatSpots = () => {
-    return (props.spots === 0 || props.spots > 1) ? "spots remaining" : "spot remaining";
-  }
+    return props.spots === 0 || props.spots > 1
+      ? "spots remaining"
+      : "spot remaining";
+  };
 
   return (
     <li
@@ -20,9 +22,14 @@ export default function DayListItem(props) {
         props.setDay(props.name);
       }}
       className={dayListItemClass}
+      data-testid="day"
     >
       <h2>{props.name}</h2>
-      {props.spots && <h3>{props.spots} {formatSpots()}</h3>}
+      {props.spots && (
+        <h3>
+          {props.spots} {formatSpots()}
+        </h3>
+      )}
       {!props.spots && <h3>no {formatSpots()}</h3>}
     </li>
   );
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,6 +10,7 @@ import {
   getAllByTestId,
   getByAltText,
   getByPlaceholderText,
+  queryByText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -36,24 +37,31 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-  const { container } = render(<Application />);
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
 
-  await waitForElement(() => getByText(container, "Archie Cohen"));
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
-  const appointments = getAllByTestId(container, "appointment");
-  const appointment = appointments[0];
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
 
-  fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.click(getByAltText(appointment, "Add"));
 
-  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-    target: { value: "Lydia Miller-Jones" }
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
-  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-
-  fireEvent.click(getByText(appointment, "Save"));
-
-  console.log(prettyDOM(appointment));
-});
-
 });
