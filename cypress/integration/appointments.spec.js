e77f89570e88db169e2467530cea4cb7fbf97f30
open-meadow <rohan.varughese@protonmail.com>
2023-01-18 16:26:30 -0500

    Add tests for booking and editing appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,5 +1,36 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
+  // reset database, start webpage and check if it is loading
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
+    cy.contains("[data-testid='day']", "Monday");
+  });
+
+  it("should book an interview", () => {
+    // open appointment form
+    cy.get("[alt=Add]").first().click();
+
+    // add details and click save
+    cy.get('[data-testid="student-name-input"]').type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
+    
+    // check if new appointment shows up
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    // open appointment form
+    cy.get("[alt=Edit]").first().click({force: true});
+
+    // change details and click save
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    
+    // check if new appointment shows up
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   })
-})
+});
