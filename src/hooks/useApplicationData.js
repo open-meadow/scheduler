95d0a2dc795c10ba8652651ef83af56f390007e3
open-meadow <rohan.varughese@protonmail.com>
2023-01-18 20:23:23 -0500

    Add new URL for webSocket

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -58,7 +58,7 @@ export default function useApplicationData() {
   // useRef makes it so event only happens once. it can be referenced but not changed
   const ws = useRef(null);
   useEffect(() => {
-    ws.current = new WebSocket("ws://localhost:8001", "protocolOne");
+    ws.current = new WebSocket("wss://scheduler-api-production-3e53.up.railway.app/", "protocolOne");
     return () => {
       ws.current.close();
     };
